5319aab2ec44e3515a730cd555cfb0624679e7ce
Sofia Modig <sofiamodig@proton.me>
2023-12-17 13:43:13 +0100

    fix styles loading

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
+function MyDocument() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
+
+MyDocument.getInitialProps = async (ctx: any) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
+
+export default MyDocument;

5839377e4ff3a8b2144e4fc25e9fd9de2e08e1ec
Sofia Modig <sofiamodig@Sofias-MacBook-Pro.local>
2026-01-22 16:28:11 +0100

    update leaderboard every 20 min

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -179,8 +179,8 @@ export default function Leaderboard({
             closer to the oscars.
           </Paragraph> */}
           <Paragraph size="sm">
-            {/* The leaderboard is updated every 10 minutes. */}
-            The leaderboard is updated once per day.
+            The leaderboard is updated every 20 minutes.
+            {/* The leaderboard is updated once per day. */}
           </Paragraph>
           {selectedYear === "all" && (
             <Paragraph size="sm">
